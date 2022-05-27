e30ab50187f623b607b78536c9bf245d26503bdc
Juamez <juanmendezs.a@gmail.com>
2022-05-27 15:29:46 -0500

    add className interactive to change mode color

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import './App.css'
 function App() {
   const [todo, setTodo] = useState()
   const [inputState, setInputState] = useState('')
-
+  const [changeTheme, setChangeTheme] = useState(true)
   function handleChange(event) {
     setInputState(event.target.value)
     console.log(inputState)
   }
 
+  function changeThemeMode() {
+    setChangeTheme(prev => !prev )
+    console.log(changeTheme)
+  }
+
   return (
+    <div className={changeTheme ? 'dark-mode' : 'light-mode'}>
     <div className="App">
       <header>
-        <img src="" alt="" />
-        <button>change mode</button>
+        <h1>todo</h1>
+        <button className="btn-dark" onClick={changeThemeMode}/>
       </header>
       <div>
         <input type="checkbox" name="" id="" aria-label=''/>
@@ -34,6 +40,7 @@ function App() {
         <button>Completed</button>
       </div>
       <p>Drag and drop to reorder list</p>
+    </div>
     </div>
   )
 }
