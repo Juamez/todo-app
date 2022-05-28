3747195195d8bcaa7bf26b7c8cf52ebb33fad773
Juamez <juanmendezs.a@gmail.com>
2022-05-27 19:57:12 -0500

    add component note

---
+++ src/Note.jsx
@@ -0,0 +1,12 @@
+
+export function Note ({inputState, noteMessage}) {
+  return (
+    <div className="container todo-list">
+      <input type="checkbox" aria-label='' />
+      <p>
+        {noteMessage}
+      </p>
+      <button />
+    </div>
+  )
+}
