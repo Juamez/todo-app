5c0a5e38ac477157f3d93b8b76a1d4f328f8fb9a
Juamez <juanmendezs.a@gmail.com>
2022-05-27 20:01:49 -0500

    import note component and implement a todo state to render an default array of objectswhich contains and id and a message

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,67 @@
 import {useState} from 'react'
+import {Note} from './Note'
 import './App.css'
 
 function App() {
-  const [todo, setTodo] = useState()
+  const [todo, setTodo] = useState([{
+    id: 0,
+    message: 'Read for 1 hour' 
+  },
+  {
+    id: 1,
+    message: 'Jog around the park 3x'
+  }
+  ])
   const [inputState, setInputState] = useState('')
   const [changeTheme, setChangeTheme] = useState(true)
   function handleChange(event) {
     setInputState(event.target.value)
-    console.log(inputState)
   }
 
   function changeThemeMode() {
-    setChangeTheme(prev => !prev )
-    console.log(changeTheme)
+    setChangeTheme(prev => !prev)
+  }
+
+  function addNote(){
+    setTodo(note => ([{
+      ...note,
+      id: note.id + 1,
+      message: inputState
+    }
+    ]))
+    console.log(todo)
   }
 
   return (
-    <div className={changeTheme ? 'dark-mode' : 'light-mode'}>
-    <div className="App">
+    <div className={changeTheme ? 'App dark-mode' : 'App light-mode'}>
       <header>
         <h1>todo</h1>
-        <button className="btn-dark" onClick={changeThemeMode}/>
+        <button className={changeTheme ? 'btn btn-dark' : 'btn btn-light'} onClick={changeThemeMode}/>
       </header>
-      <div>
-        <input type="checkbox" name="" id="" aria-label=''/>
+      <div className="input-todo">
         <input 
           type="text" 
           onChange={handleChange}
           aria-label='' 
-          placeholder="Create a new todo"
+          placeholder="Create a new todo..."
         />
+      
       </div>
-      <div>
-        {inputState}
+      <div className="container">      
+        {todo.map(elem => <Note key={elem.id} noteMessage={elem.message}/>)}
+        <div className="container msg">
+          <p>{todo.length} items left</p>
+          <button>Clear completed</button>
+        </div>
       </div>
-      <div>
+      <div className="container btns-filter">
         <button>All</button>
         <button>Active</button>
         <button>Completed</button>
       </div>
-      <p>Drag and drop to reorder list</p>
-    </div>
+      <footer className="footer">
+          <p>Drag and drop to reorder list</p>
+      </footer>
     </div>
   )
 }
